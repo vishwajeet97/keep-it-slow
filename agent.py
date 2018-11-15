f6d71c6130f2f514f369932dc4a8f16a87d0cbfd
Harshith Goka <harshith9399@gmail.com>
2018-11-15 18:27:14 +0530

    add tiling code; move to parameterized Q

--- agent.py
+++ agent.py
@@ -1,10 +1,13 @@
 import numpy as np
-import gym 
+import gym
+import tiles
+
+
 class BaseAgent:
     def __init__(self, env, epochs=1000):
         self.env = env
         self.epochs = epochs
-    
+
     def learn(self):
         ''' 
 
@@ -14,15 +17,16 @@ class BaseAgent:
             self.t = 0
             while True:
                 self.env.render()
-                import pdb; pdb.set_trace()
+                # import pdb
+                # pdb.set_trace()
                 action = self.getAction(self.state)
                 new_state, reward, done, info = self.env.step(action)
-                self.update(self.state, action, reward, new_state, t)
+                self.update(self.state, action, reward, new_state, self.t)
                 self.state = new_state
-                t += 1
+                self.t += 1
                 if done:
                     self.episodeEnded()
-                    print("Episode finished after {} timesteps".format(t+1))
+                    print("Episode finished after {} timesteps".format(self.t+1))
                     break
 
     def getAction(self, state):
@@ -39,23 +43,35 @@ class BaseAgent:
 
     def episodeEnded(self):
         pass
-    
+
 
 class ExperienceReplayAgent(BaseAgent):
-    def __init__(self, env: gym.Env, epsilon = 0.1, N = 10):
+    def __init__(self, env: gym.Env, epsilon=0.1, N=10, M=100, num_tilings=16, tile_dim=4, gamma = 1.):
         super(ExperienceReplayAgent, self).__init__(env)
         self.experience = []
-        self.Q = np.zeros(1)
         self.epsilon = epsilon
         self.num_episodes = 0
+
         self.N = N
-    
+        self.M = M
+
+        self.num_tilings = num_tilings
+        
+        self.tile_dim = tile_dim
+        self.grid_size = tile_dim ** np.prod(self.env.observation_space.shape)
+
+        self.weights = np.random.randn(num_tilings * self.grid_size)
+        self.scale = tile_dim / (self.env.observation_space.high - self.env.observation_space.low)
+
+        self.alpha = 0.5
+        self.gamma = gamma
+
     def getAction(self, state):
         '''
 
         '''
         if np.random.binomial(1, self.epsilon) == 0:
-            return np.argmax(self.Q[state, :])
+            return np.argmax([self.getQValue(state, action) for action in range(self.env.action_space.n)])
         else:
             return self.env.action_space.sample()
 
@@ -64,15 +80,35 @@ class ExperienceReplayAgent(BaseAgent):
 
         '''
         self.experience.append((state, action, reward, new_state))
-    
+
     def episodeEnded(self):
         '''
 
         '''
         self.num_episodes += 1
         if (self.num_episodes % self.N == 0):
-            self.batch_update()
-    
+            for i in range(self.M):
+                self.batch_update()
+
+    def getFeatures(self, state, action):
+        feat = np.zeros(self.grid_size * self.num_tilings)
+        tileIndex = tiles.tiles(
+            self.num_tilings, self.grid_size * self.num_tilings, state * self.scale, [action])
+        feat[tileIndex] = 1
+        return feat
+
+    def getQValue(self, state, action):
+        tileIndices = tiles.tiles(
+            self.num_tilings, self.grid_size * self.num_tilings, state * self.scale, [action])
+        return np.sum(self.weights[tileIndices])
+
     def batch_update(self):
         state, action, reward, new_state = np.random.choice(self.experience)
-        self.Q[state, ]
+        phi_k = self.getFeatures(state, action)
+        Q_k = self.getQValue(state, action)
+
+        Q_k_plus_1_max = np.max( [ self.getQValue(new_state, a) for a in range(self.env.action_space.n)] )
+        
+        delta_k = reward + self.gamma * Q_k_plus_1_max - Q_k
+
+        self.weights = self.weights + self.alpha * (delta_k) * phi_k
