bce2ef5835507446161a1870720d6759ca779b96
vishwajeet97 <vishwajeetsingh29797@gmail.com>
2018-11-17 21:08:40 +0530

    Implemented Reinforce Algorithm

--- agent.py
+++ agent.py
@@ -189,3 +189,96 @@ class FittedQIterationAgent(ExperienceReplayAgent):
             model = model.fit(X, y)
         
         self.Q = model
+
+
+class ReinforceAgent(BaseAgent):
+    def __init__(self, env: gym.Env, epochs=4000, epsilon=0.05, N=100, M=150, num_tilings=32, tile_dim=8, gamma=1.):
+        super(ReinforceAgent, self).__init__(env, epochs=epochs)
+        self.experience = []
+        self.epsilon = epsilon
+        self.num_episodes = 0
+
+        self.N = N
+        self.M = M
+
+        self.num_tilings = num_tilings
+
+        self.tile_dim = tile_dim
+        self.grid_size = tile_dim ** np.prod(self.env.observation_space.shape)
+
+        self.policy_weights = np.zeros((num_tilings * self.grid_size, self.env.action_space.n))
+        self.gradients = np.zeros((num_tilings * self.grid_size, self.env.action_space.n)) 
+        self.value_weights = np.zeros(num_tilings * self.grid_size)
+        import pdb
+        pdb.set_trace()
+        self.scale = tile_dim / \
+            (self.env.observation_space.high - self.env.observation_space.low)
+
+        self.alpdecay = 1
+        self.alpha = 0.2
+        self.gamma = gamma
+
+    def getAction(self, state):
+        '''
+
+        '''
+        if np.random.binomial(1, self.epsilon) == 0:
+            return np.argmax([np.exp(np.dot(self.getFeaturesPolicy(state, action), self.policy_weights)) for action in range(self.env.action_space.n)])
+        else:
+            return self.env.action_space.sample()
+
+    def getValue(self, state):
+        return np.dot(self.getFeaturesValue(state), self.value_weights)
+
+    def updatePolicy(self, state, action, reward, new_state):
+        feat = self.getFeaturesPolicy(state, action)
+
+        gradient = np.zeros(self.num_tilings * self.grid_size)
+        denominator = 0
+        for actionDash in range(self.env.action_space.n):
+            feat_actionDash = self.getFeaturesPolicy(state, actionDash)
+            X_difference = feat - feat_actionDash
+
+            temp = np.exp(np.dot(feat_actionDash, self.policy_weights[:, actionDash]))
+            gradient += temp * X_difference
+            denominator += temp
+
+        gradient = gradient / denominator
+
+        self.gradients[:, action] += self.alpdecay * (reward + self.gamma * self.getValue(new_state) - self.getValue(state)) * gradient
+
+    def updateValue(self, state, action, reward, new_state):
+        self.value_weights += self.alpdecay * (reward + self.gamma * self.getValue(new_state) - self.getValue(state)) * self.getFeaturesValue(state)
+
+    def update(self, state, action, reward, new_state, timestep, terminal_state):
+        '''
+
+        '''
+        if not terminal_state:
+            self.updateValue(state, action, reward, new_state)
+            if timestep % 10 == 0:
+                self.updatePolicy(state, action, reward, new_state)
+
+    def episodeEnded(self):
+        '''
+
+        '''
+        self.num_episodes += 1
+        self.policy_weights += self.gradients
+        self.gradients[:, :] = 0        
+
+    def getFeaturesValue(self, state):
+        feat = np.zeros(self.grid_size * self.num_tilings)
+        tileIndex = tiles.tiles(
+            self.num_tilings, self.grid_size * self.num_tilings, state * self.scale)
+        feat[tileIndex] = 1
+
+        return feat
+
+    def getFeaturesPolicy(self, state, action):
+        feat = np.zeros(self.grid_size * self.num_tilings)
+        tileIndex = tiles.tiles(
+            self.num_tilings, self.grid_size * self.num_tilings, state * self.scale, [action])
+        feat[tileIndex] = 1
+
+        return feat
